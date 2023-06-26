c6fe92be34ef32b05ff311fdec6c3300b5ed224e
moseoh <azqazq195@gmail.com>
2023-06-27 02:47:17 +0900

    refactor: contentvec channels 및 경로 수정

--- script/update-config.py
+++ script/update-config.py
@@ -7,8 +7,8 @@ with open("DDSP-SVC/configs/combsub.yaml", "r") as file:
     data = yaml.safe_load(file)
 
 data['data']['encoder'] = "contentvec"
-data['data']['encoder_out_channels'] = 768
-data['data']['encoder_ckpt'] = "pretrain/hubert/checkpoint_best_legacy_500.pt"
+data['data']['encoder_out_channels'] = 256
+data['data']['encoder_ckpt'] = "pretrain/contentvec/checkpoint_best_legacy_500.pt"
 data['train']['cache_device'] = "cuda"
 data['train']['num_workers'] = 2
 data['train']['batch_size'] = batch_size
